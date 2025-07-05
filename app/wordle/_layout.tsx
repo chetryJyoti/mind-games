d7db67deae91b57c2cf93b53f199116eb8933e12
jyoti chetry <jyotichetry087@gmail.com>
2025-07-05 23:07:18 +0530

    feat: implement game layout boxes

--- app/wordle/_layout.tsx
+++ app/wordle/_layout.tsx
@@ -1,13 +1,26 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { useColorScheme } from "react-native";
 
-const _layout = () => {
+const WordleLayout = () => {
+  const colorScheme = useColorScheme();
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="game" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="game"
+        options={{
+          headerShown: true,
+          headerBackTitle: "Home",
+          headerTintColor: colorScheme === "dark" ? "#fff" : "#000",
+          headerBackTitleStyle: {
+            fontSize: 16,
+            fontFamily: "FrankRuhlLibre_800ExtraBold",
+          },
+        }}
+      />
     </Stack>
   );
 };
 
-export default _layout;
+export default WordleLayout;
--- app/wordle/game.tsx
+++ app/wordle/game.tsx
@@ -1,14 +1,97 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import OnScreenKeyboard from "@/components/OnScreenKeyboard";
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { Stack } from "expo-router";
+import React, { useState } from "react";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+
+const ROWS = 6;
 
 const Game = () => {
+  const colorScheme = useColorScheme();
+  const backgroundColor = Colors[colorScheme ?? "light"].gameBg;
+  const textColor = Colors[colorScheme ?? "light"].text;
+  const grayColor = Colors[colorScheme ?? "light"].gray;
+
+  const [rows, setRows] = useState<string[][]>(
+    new Array(ROWS).fill(new Array(5).fill("s"))
+  );
+  const [currentRow, setCurrentRow] = useState(0);
+  const [currentColumn, setCurrentColumn] = useState(0);
+
+  const [greenLetters, setGreenLetters] = useState<string[]>([]);
+  const [yellowLetters, setYellowLetters] = useState<string[]>([]);
+  const [grayLetters, setGrayLetters] = useState<string[]>([]);
+
+  const addKey = (key: string) => {
+    console.log(`Adding key: ${key}`);
+    // todo
+  };
+
+  console.log(rows);
+
   return (
-    <View>
-      <Text>Game</Text>
+    <View style={[styles.container, { backgroundColor }]}>
+      <Stack.Screen
+        options={{
+          headerRight: () => (
+            <View style={styles.headerIcons}>
+              <Ionicons
+                name="help-circle-outline"
+                size={28}
+                color={textColor}
+              />
+              <Ionicons name="podium-outline" size={24} color={textColor} />
+              <Ionicons name="settings-sharp" size={24} color={textColor} />
+            </View>
+          ),
+        }}
+      />
+      <View style={styles.gameField}>
+        {rows.map((row, rowIndex) => (
+          <View style={styles.gameFieldRow} key={`row-${rowIndex}`}>
+            {row.map((cell, cellIndex) => (
+              <View style={styles.cell} key={`cell-${rowIndex}-${cellIndex}`}>
+                <Text style={styles.cellText}>{cell}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
+      <OnScreenKeyboard />
     </View>
   );
 };
 
 export default Game;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 40,
+  },
+  headerIcons: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  gameField: {
+    alignItems: "center",
+    gap: 8,
+  },
+  gameFieldRow: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  cell: {
+    width: 60,
+    height: 60,
+    borderWidth: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cellText: {
+    fontSize: 30,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
